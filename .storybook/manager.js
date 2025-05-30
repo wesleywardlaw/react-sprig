7ee51eeefddcefd8d1d8b0f1b643c05b65260f5c
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-05-30 14:48:04 -0500

    feat: change storybook logo

---
+++ .storybook/manager.js
@@ -0,0 +1,16 @@
+import { addons } from '@storybook/manager-api'
+import { create } from '@storybook/theming/create'
+
+const customTheme = create({
+  base: 'light',
+  brandTitle: 'react-sprig',
+  brandUrl: 'https://react-sprig.netlify.app',
+  brandImage: 'logo.png',
+  brandTarget: '_self',
+})
+
+addons.setConfig({
+  theme: customTheme,
+})
+
+console.log('hello')
